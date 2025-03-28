b9b87cb3a5d461245238999038d9f2e156846462
michael <michaelferreiraalves2001@gmail.com>
2025-03-28 11:02:31 -0300

    feito

--- settings.py
+++
@@ -1,7 +0,0 @@
-import os, dotenv
-
-dotenv.load_dotenv()
-
-EVOLUTION_API_URL = os.getenv('EVOLUTION_API_URL')
-EVOLUTION_API_TOKEN = os.getenv('EVOLUTION_API_TOKEN')
-EVOLUTION_INSTANCE = os.getenv('EVOLUTION_INSTANCE')
